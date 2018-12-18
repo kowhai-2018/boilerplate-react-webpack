3bed40bc631d129795ce3c2cbe5321fef067bb0a
kiri-tavaga <kiri.tavaga@icloud.com>
2018-12-19 12:44:13 +1300

    added semantic-ui-react and switch paths

---
+++ client/components/Main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Home from './home'
+import Practice from './Practice'
+import Test from './Test'
+import Challenge from './Challenge'
+import Contact from './Contact'
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
+
+export default class Main extends React.Component {
+  render () {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/practice' component={Practice} />
+          <Route path='/test' component={Test} />
+          <Route path='/challenge' component={Challenge} />
+          <Route path='/contact' component={Contact} />
+        </Switch>
+      </Router>
+    )
+  }
+}
