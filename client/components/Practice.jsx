19798c6cb35963eeeef1d2da02a5bba55163a59a
kiri-tavaga <kiri.tavaga@icloud.com>
2018-12-19 13:08:22 +1300

    set up practice component

---
+++ client/components/Practice.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Practice extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Practice</h1>
+      </div>
+    )
+  }
+}
